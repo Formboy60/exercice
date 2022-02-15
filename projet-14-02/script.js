79c27a15aa7304a826d2b9520ee979a6c4cd74ad
formboy60 <formboy@gmail.com>
2022-02-15 16:30:13 +0100

    maj-exo

--- projet-14-02/script.js
+++ projet-14-02/script.js
@@ -1,22 +1,35 @@
 
+const searchBar = document.getElementById('searchBar')
+let tab = []
 
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();    
+    const filteredTitle = tab.filter((hub) => {
+        
+        return (
+            hub.attributes.title.toLowerCase().includes(searchString) ||
+            hub.attributes.category.toLowerCase().includes(searchString)
+            );
+        });
+        myApi(filteredTitle);
+    })
 
 
 
 fetch("https://strapi-gogokodo.herokuapp.com/api/sources")
 .then(response => response.json())
-.then(response => myApi(response.data))
+.then(response => {myApi(response.data); tab=response.data})
 .catch(error => console.log("Erreur : " + error));
 
 
 
 function myApi(data) {
     const article = document.getElementById("art")
-console.log(data)
+    article.innerHTML=''
 
 for(video of data){
-    console.log(video.attributes.title)
-    
+      
     
 article.innerHTML += `<section class="caps"> 
 <h3>${video.attributes.title}</h3>
@@ -25,7 +38,6 @@ article.innerHTML += `<section class="caps">
 
 }
 }
-
 
 
 
